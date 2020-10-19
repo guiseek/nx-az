79ecb217339e4e90b214648cf74a594855ed3255
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-10-19 15:40:54 -0300

    chore(repo): chore: commitizen

---
+++ .cz-config.js
@@ -0,0 +1,86 @@
+module.exports = {
+  types: [
+    { value: 'feat', name: 'feat:     A new feature' },
+    { value: 'fix', name: 'fix:      A bug fix' },
+    { value: 'docs', name: 'docs:     Documentation only changes' },
+    {
+      value: 'cleanup',
+      name:
+        'cleanup:  A code change that neither fixes a bug nor adds a feature',
+    },
+    {
+      value: 'chore',
+      name: "chore:    Other changes that don't modify src or test files",
+    },
+    { value: 'wip', name: 'wip:      Trabalho em progresso' },
+    { value: 'ci', name: 'ci:       Integração contínua / Pipeline / Esteira' },
+  ],
+
+  scopes: [
+    { name: 'main-front', description: 'anything Main Front scope' },
+    {
+      name: 'main-front-admin',
+      description: 'anything Main Front Admin specific',
+    },
+    { name: 'main-api', description: 'anything Main API scope' },
+    { name: 'api-admin', description: 'anything API Admin specific' },
+    { name: 'admin', description: 'anything Admin specific' },
+    { name: 'articles', description: 'anything Article specific' },
+    { name: 'server', description: 'anything Server specific' },
+    { name: 'ui-kit', description: 'anything UiKit specific' },
+    { name: 'chat-panel', description: 'anything ChatPanel specific' },
+    { name: 'ngex-crud', description: 'anything NgCrud specific' },
+    { name: 'docs', description: 'anything related to docs infrastructure' },
+    {
+      name: 'testing',
+      description:
+        'anything testing specific (e.g., jest, cypress or util-mock)',
+    },
+    {
+      name: 'repo',
+      description: 'anything related to managing the repo itself',
+    },
+    { name: 'misc', description: 'misc stuff' },
+  ],
+
+  allowTicketNumber: false,
+  isTicketNumberRequired: false,
+  ticketNumberPrefix: 'TICKET-',
+  ticketNumberRegExp: '\\d{1,5}',
+
+  // it needs to match the value for field type. Eg.: 'fix'
+  scopeOverrides: {
+    fix: [
+      { name: 'merge' },
+      { name: 'style' },
+      { name: 'e2eTest' },
+      { name: 'unitTest' },
+    ],
+  },
+  // override the messages, defaults are as follows
+  messages: {
+    type: 'Selecione o tipo de alteração que você está enviando:',
+    scope: '\nIndique o ESCOPO desta alteração (opcional):',
+    // used if allowCustomScopes is true
+    customScope: 'Indique o escopo desta alteração:',
+    subject: 'Escreva uma descrição CURTA e IMPERATIVA da mudança:\n',
+    body:
+      'Forneça uma descrição mais detalhada da alteração (optional). Use "|" para quebras de linha:\n',
+    breaking:
+      'Listar quaisquer ALTERAÇÕES DE QUEBRA / BREAKING CHANGES (opcional):\n',
+    footer:
+      'Liste quaisquer conclusões de PROBLEMAS / ISSUES por esta alteração (opcional). Ex.: #31, #34:\n',
+    confirmCommit: 'Tem certeza de que deseja continuar com o commit acima?',
+  },
+
+  allowCustomScopes: true,
+  allowBreakingChanges: ['feat', 'fix'],
+  // skip any questions you want
+  skipQuestions: ['body'],
+
+  // limit subject length
+  subjectLimit: 100,
+  // breaklineChar: '|', // It is supported for fields body and footer.
+  // footerPrefix : 'ISSUES CLOSED:'
+  // askForBreakingChangeFirst : true, // default is false
+}
