c7fba98f985d829e8d15acd30c26d2dbced3171e
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-10-25 20:08:52 -0300

    feat(nx-fire): fire auth guards

---
+++ apps/exp/auth/src/app/admin/admin.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { Routes, RouterModule } from '@angular/router'
+import { AdminComponent } from './admin.component'
+
+const routes: Routes = [{ path: '', component: AdminComponent }]
+
+@NgModule({
+  declarations: [AdminComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+})
+export class AdminModule {}
